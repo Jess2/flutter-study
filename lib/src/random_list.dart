2f48f95ee7339006d10bfed298281c5b39555511
Jess2 <jess2.developer@gmail.com>
2021-02-14 18:23:18 +0900

    feat: 랜덤 단어를 리스트 형식으로 출력

--- lib/src/random_list.dart
+++ lib/src/random_list.dart
@@ -9,14 +9,33 @@ class RandomList extends StatefulWidget {
 }
 
 class _RandomListState extends State<RandomList> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final Set<WordPair> _saved = Set<WordPair>();
+
   @override
   Widget build(BuildContext context) {
-    final randomWord = WordPair.random();
     return Scaffold(
-        appBar: AppBar(
-            title: Text("Jess2")
-        ),
-        body: Center(child: Text(randomWord.asPascalCase, textScaleFactor: 1.5,))
+      appBar: AppBar(title: Text("Jess2")),
+      body: ListView.builder(itemBuilder: (context, index) {
+        // 0, 2, 4, ... : real items
+        // 1, 3, 5, ... : dividers
+        if (index.isOdd) {
+          return Divider();
+        }
+
+        var realIndex = index ~/ 2;
+
+        if (realIndex >= _suggestions.length) {
+          _suggestions.addAll(generateWordPairs().take(10));
+        }
+
+        return ListTile(
+            title: Text(
+          _suggestions[realIndex].asPascalCase,
+          textScaleFactor: 1.5,
+        ));
+      }),
+      // body: Center(child: Text(randomWord.asPascalCase, textScaleFactor: 1.5,))
     );
   }
 }

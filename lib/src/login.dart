fd1f4d9e41f51725ed17d7168ab76c1d8de0b393
JESS2 <jess2.developer@gmail.com>
2021-02-27 22:33:27 +0900

    feat: 로그인 버튼 누르면 랜덤 리스트 페이지로 이동하도록 처리

--- lib/src/login.dart
+++ lib/src/login.dart
@@ -1,3 +1,4 @@
+import 'package:first_flutter_app/src/random_list.dart';
 import 'package:flutter/material.dart';
 
 class Login extends StatefulWidget {
@@ -21,24 +22,26 @@ class _LoginState extends State<Login> with ValidationMixin {
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
             Container(
-              margin: EdgeInsets.only(bottom: 30.0),
-              color: Colors.blue,
-              width: 100.0,
-              height: 100.0,
-              child: Text('logo',
-                style: TextStyle(
-                  color: Colors.white,
-                  fontSize: 18,
-                ),
-                textAlign: TextAlign.center,
-              )
-            ),
+                margin: EdgeInsets.only(bottom: 30.0),
+                color: Colors.blue,
+                width: 100.0,
+                height: 100.0,
+                child: Text(
+                  'logo',
+                  style: TextStyle(
+                    color: Colors.white,
+                    fontSize: 18,
+                  ),
+                  textAlign: TextAlign.center,
+                )),
             Container(
-              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 10.0, bottom: 10.0),
+              margin: EdgeInsets.only(
+                  left: 50.0, right: 50.0, top: 10.0, bottom: 10.0),
               child: emailField(),
             ),
             Container(
-              margin: EdgeInsets.only(left: 50.0, right: 50.0, top: 10.0, bottom: 20.0),
+              margin: EdgeInsets.only(
+                  left: 50.0, right: 50.0, top: 10.0, bottom: 20.0),
               child: passwordField(),
             ),
             Container(
@@ -85,15 +88,18 @@ class _LoginState extends State<Login> with ValidationMixin {
     return RaisedButton(
       color: Colors.blue,
       child: Text('Sign In',
-        style: TextStyle(
-          color: Colors.white,
-          fontSize: 18,
-        )
-      ),
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 18,
+          )),
       onPressed: () {
         if (formKey.currentState.validate()) {
           formKey.currentState.save();
-          print('time to pose $email and $password to my API');
+          Navigator.of(context).push(MaterialPageRoute(
+            builder: (context) => RandomList(),
+          )).then((value) {
+            setState(() {});
+          });
         }
       },
     );
